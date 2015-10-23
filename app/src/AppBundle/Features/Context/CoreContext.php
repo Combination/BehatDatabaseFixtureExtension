a6f5aa9997694e2bda3b77b0a17379436356f493
ReenExe <ipourri@gmail.com>
2015-10-23 19:35:05 +0300

    method: Truncated tables

--- app/src/AppBundle/Features/Context/CoreContext.php
+++ app/src/AppBundle/Features/Context/CoreContext.php
@@ -3,7 +3,9 @@
 namespace AppBundle\Features\Context;
 
 use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -20,5 +22,28 @@ class CoreContext implements Context, KernelAwareContext
     public function setKernel(KernelInterface $kernel)
     {
         $this->container = $kernel->getContainer();
+    }
+
+    /**
+     * @param string $table
+     */
+    protected function clearTable($table)
+    {
+        /** @var Connection $connection */
+        $connection = $this->container->get('doctrine')->getConnection();
+        $connection->query("TRUNCATE TABLE $table");
+    }
+
+    /**
+     * @Given /^Truncated tables$/
+     * @param TableNode $table
+     */
+    public function thereAreInDbTruncatedTables(TableNode $table)
+    {
+        foreach ($table->getTable() as $tableNames) {
+            foreach ($tableNames as $tableName) {
+                $this->clearTable($tableName);
+            }
+        }
     }
 }
--- app/src/AppBundle/Features/Fixtures/fixtures.feature
+++ app/src/AppBundle/Features/Fixtures/fixtures.feature
@@ -1,2 +1,6 @@
 @fixtures
 Feature: Load basic project
+
+  Scenario: Truncate table
+      Given Truncated tables
+          | f_users |
