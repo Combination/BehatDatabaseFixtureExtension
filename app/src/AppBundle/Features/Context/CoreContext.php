07480ff6357b0dec000b125c2fd854899853b112
ReenExe <ipourri@gmail.com>
2015-10-23 19:57:12 +0300

    assert record exists in table

--- app/src/AppBundle/Features/Context/CoreContext.php
+++ app/src/AppBundle/Features/Context/CoreContext.php
@@ -5,6 +5,8 @@ namespace AppBundle\Features\Context;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\DBAL\Connection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -38,7 +40,7 @@ class CoreContext implements Context, KernelAwareContext
     }
 
     /**
-     * @Given /^There are items in table \'([^\']+)\'$/
+     * @Given /^There are records in table \'([^\']+)\'$/
      * @param string $tableName
      * @param TableNode $table
      */
@@ -63,6 +65,33 @@ class CoreContext implements Context, KernelAwareContext
 
         foreach ($table as $row) {
             $sth->execute($row);
+        }
+    }
+
+    /**
+     * @Given /^Records in \'([^\']+)\' where \'([^\']+)\', has data$/
+     * @param $tableName
+     * @param $where
+     * @param TableNode $table
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Exception
+     */
+    public function thereAreRecordsInWhere($tableName, $where, TableNode $table)
+    {
+        /** @var Connection $connection */
+        $connection = $this->container->get('doctrine')->getConnection();
+
+        $exists = new ArrayCollection($connection->query("SELECT * FROM $tableName WHERE $where")->fetchAll());
+        $criteria = new Criteria();
+        $recordsMatching = $exists->matching($criteria);
+        $record = $recordsMatching->first();
+        $i = 1;
+        foreach ($table as $row) {
+            foreach($row as $key => $value) {
+                \PHPUnit_Framework_Assert::assertArrayHasKey($key, $record, "Column '$key' doesn't exists in database");
+                \PHPUnit_Framework_Assert::assertEquals($value, $record[$key], "Invalid value for '$key' in row $i");
+            }
+            ++$i;
         }
     }
 
--- app/src/AppBundle/Features/Fixtures/fixtures.feature
+++ app/src/AppBundle/Features/Fixtures/fixtures.feature
@@ -4,6 +4,9 @@ Feature: Load basic project
   Scenario: Truncate table
       Given Truncated tables
           | f_users |
-      And There are items in table 'f_users'
+      And There are records in table 'f_users'
+          | id | name |
+          | 1  | Alex |
+      And Records in 'f_users' where 'id = 1', has data
           | id | name |
           | 1  | Alex |
